845769240b16f6bf57586d3d09e5e455ecddc4cf
Warnita Adams <warnita.adams.rsa@gmail.com>
2019-03-19 14:25:50 +0200

    update jQuery

---
+++ JS/jQuery.js
@@ -0,0 +1,11 @@
+// Jquery (the open source Javscipt Libary) allows you to harness the power of JS to acomplish a mytaid of awesome things on your webpage. you might :
+// add, delete , or modfify HTML elements within your page
+
+// Change the styles of the elements on the page bt modifying their associated CSS.
+
+// Animate elements on your page.
+
+// Send and recieve data from a serverr via AJAX (Asynchronous JavaScript And XML) so your page doesnt have to be reloaded after submitting form.
+
+
+
